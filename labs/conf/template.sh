437f7f2cc146c6c50c92eb940e2165b5e7a00882
car <carlos.echc11@gmail.com>
2021-11-18 21:10:51 -0600

    shell docker

--- labs/conf/template.sh
+++ labs/conf/template.sh
@@ -11,9 +11,10 @@ chown ${username}:${username} /home/${username}/.ssh/authorized_keys
 chmod 600  /home/${username}/.ssh/authorized_keys
 
 # docker
-curl -fsSL https://get.docker.com -o get-docker.sh | sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 sudo usermod -aG docker ${username}
-newgrp docker
+# newgrp docker
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
