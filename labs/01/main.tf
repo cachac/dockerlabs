d4af958b91239595f385c38e8bac5cd5173208b8
car <carlos.echc11@gmail.com>
2021-11-20 14:54:13 -0600

    local-ip

--- labs/01/main.tf
+++ labs/01/main.tf
@@ -18,6 +18,10 @@ output "lab-ssh" {
   value = "ssh ${var.username}@${google_compute_instance.lab[0].network_interface.0.access_config.0.nat_ip} -i ~/Documents/code/dockerlabs/labs/keys/prod/dockerlabkey"
 }
 
+output "local-ip" {
+	value = google_compute_instance.lab[0].network_interface.0.network_ip
+}
+
 output "IP-list" {
   value = "${google_compute_instance.lab[*].network_interface.0.access_config.0.nat_ip}"
 }
