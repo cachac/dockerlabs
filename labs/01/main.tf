370576b8f7f6d9adf8b16c029a9f86b785fbc536
car <carlos.echc11@gmail.com>
2021-11-20 14:41:38 -0600

    output

--- labs/01/main.tf
+++ labs/01/main.tf
@@ -14,8 +14,15 @@ resource "local_file" "ssh_public_key_pem" {
   file_permission   = "0600"
 }
 
-output "labs-external-IP" {
+output "lab-ssh" {
   value = "ssh ${var.username}@${google_compute_instance.lab[0].network_interface.0.access_config.0.nat_ip} -i ~/Documents/code/dockerlabs/labs/keys/prod/dockerlabkey"
 }
 
+output "IP-list" {
+  value = "${google_compute_instance.lab[*].network_interface.0.access_config.0.nat_ip}"
+}
+
+output "names" {
+  value = google_compute_instance.lab[*].name
+}
 
