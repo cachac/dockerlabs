48ab8110f1b6e5c4210952ea953a37ff42d21bfc
Carlos Ch <carlos.echc11@gmail.com>
2023-05-05 12:29:20 -0600

    dev

--- dev/api/src/routes/index.js
+++ dev/api/src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const router = express.Router();
 const { APP_NAME, APP_VERSION } = process.env;
+const sticky = (Math.random() + 1).toString(36).substring(10);
 
 router.get("/healthcheck", (_, res) => {
   res.send({
     app: APP_NAME || "Dockerlabs api",
     version: APP_VERSION || "1.x.x",
+    sticky
   });
 });
 
