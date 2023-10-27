d1e0144928839912bee81d505900b54e4abb5de4
Carlos Ch <carlos.echc11@gmail.com>
2023-10-26 23:21:11 -0600

    v3

--- dev/frontend/plugins/containerID.js
+++ dev/frontend/plugins/containerID.js
@@ -1,6 +1,6 @@
 import { getContainerIdSync } from 'docker-id'
 
-const id = getContainerIdSync()
+const id = getContainerIdSync() || Math.random().toString(36).substring(2, 15)
 const isDocker = Boolean(id)
 // eslint-disable-next-line no-console
 console.log('isDocker :>> ', isDocker)
